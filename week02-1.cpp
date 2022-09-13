087737922e25923b456413858050da350a6a32f0
UranusYang <112918915+UranusYang@users.noreply.github.com>
2022-09-13 12:04:09 +0800

    week02_int(integer)

---
+++ week02-1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("H\tello World\n");
+    printf("\tHello World\n");
+}
---
+++ week02-2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("333\n");
+    printf("這裡有字，有插入值\n");
+    printf("這裡有字%d有插入值\n", 333);
+}
