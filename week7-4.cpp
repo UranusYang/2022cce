720ad1488e6f0f5fdd083616e61bf8d6cdb84b15
UranusYang <112918915+UranusYang@users.noreply.github.com>
2022-10-18 11:29:20 +0800

    week7-4_10進位轉2進位

---
+++ week7-4.cpp
@@ -0,0 +1,38 @@
+///筁ㄓ稱
+#include <stdio.h>
+int main()
+{
+    ///叫块9计
+    int n;
+    scanf("%d", &n);
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+
+    printf("瞷计:%d\n", n%10);
+    n = n/10;
+}
